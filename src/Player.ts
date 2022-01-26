c60ac393c326d8408e5c97dfe7e2c6d35901aaf7
Christian Klauser <christian.klauser@digitecgalaxus.ch>
2022-01-26 14:24:29 +0100

    add logs

--- src/Player.ts
+++ src/Player.ts
@@ -36,25 +36,31 @@ export class Player {
     const me = gameState.players[gameState.in_action];
     if (me.hole_cards && me.hole_cards.length === 2) {
       const bestHand = detectBestHand(me.hole_cards, gameState.community_cards);
-
+      console.error("bestHand: ", bestHand);
       if (bestHand === Hand.ThreeOfAKind) {
+        console.error("RAISE min 300 because of 3 of a kind");
         betCallback(Math.max(300, gameState.current_buy_in));
       } else if (bestHand === Hand.Pair) {
+        console.error("RAISE min 100 because of 2 of a kind");
         betCallback(Math.max(100, gameState.current_buy_in));
       } else {
         const sum = cardValue(me.hole_cards[0]) + cardValue(me.hole_cards[1]);
         if (sum > 16) {
           if(gameState.current_buy_in > me.stack * 0.5) {
+            console.error("FOLD! too rich for us");
             betCallback(0);
           }
           else {
+            console.error("CALL because of high cards");
             betCallback(gameState.current_buy_in);
           }
         } else {
+          console.error("FOLD because of low cards")
           betCallback(0);
         }
       }
     } else {
+      console.error("CALL (blind)");
       betCallback(gameState.current_buy_in);
     }
   }
