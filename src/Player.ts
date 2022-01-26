b9104e2e33eda41cbd3b4b4fb5859a181fc93cbe
Christian Klauser <christian.klauser@digitecgalaxus.ch>
2022-01-26 15:03:48 +0100

    refactor into `bet` function

--- src/Player.ts
+++ src/Player.ts
@@ -28,41 +28,47 @@ function detectBestHand(hole_cards: ICard[], community_cards: ICard[]): Hand {
   }
 }
 
+function bet(gameState: IGameState): number {
+  const me = gameState.players[gameState.in_action];
+  if (me.hole_cards && me.hole_cards.length === 2) {
+    const bestHand = detectBestHand(me.hole_cards, gameState.community_cards);
+    console.error("bestHand: ", bestHand);
+    if (bestHand === Hand.ThreeOfAKind) {
+      console.error("RAISE min 300 because of 3 of a kind");
+      return Math.max(300, gameState.current_buy_in);
+    } else if (bestHand === Hand.Pair) {
+      console.error("RAISE min 100 because of 2 of a kind");
+      return Math.max(100, gameState.current_buy_in);
+    } else {
+      const sum = cardValue(me.hole_cards[0]) + cardValue(me.hole_cards[1]);
+      if (sum > 16) {
+        if(gameState.current_buy_in > me.stack * 0.5) {
+          console.error("FOLD! too rich for us");
+          return 0;
+        }
+        else {
+          console.error("CALL because of high cards");
+          return gameState.current_buy_in;
+        }
+      } else {
+        console.error("FOLD because of low cards");
+        return 0;
+      }
+    }
+  } else {
+    console.error("CALL (blind)");
+    return gameState.current_buy_in;
+  }
+}
+
 export class Player {
   public betRequest(
     gameState: IGameState,
     betCallback: (bet: number) => void
   ): void {
-    const me = gameState.players[gameState.in_action];
-    if (me.hole_cards && me.hole_cards.length === 2) {
-      const bestHand = detectBestHand(me.hole_cards, gameState.community_cards);
-      console.error("bestHand: ", bestHand);
-      if (bestHand === Hand.ThreeOfAKind) {
-        console.error("RAISE min 300 because of 3 of a kind");
-        betCallback(Math.max(300, gameState.current_buy_in));
-      } else if (bestHand === Hand.Pair) {
-        console.error("RAISE min 100 because of 2 of a kind");
-        betCallback(Math.max(100, gameState.current_buy_in));
-      } else {
-        const sum = cardValue(me.hole_cards[0]) + cardValue(me.hole_cards[1]);
-        if (sum > 16) {
-          if(gameState.current_buy_in > me.stack * 0.5) {
-            console.error("FOLD! too rich for us");
-            betCallback(0);
-          }
-          else {
-            console.error("CALL because of high cards");
-            betCallback(gameState.current_buy_in);
-          }
-        } else {
-          console.error("FOLD because of low cards")
-          betCallback(0);
-        }
-      }
-    } else {
-      console.error("CALL (blind)");
-      betCallback(gameState.current_buy_in);
-    }
+    const ourBet = bet(gameState);
+    console.error("BET: ", ourBet);
+    betCallback(ourBet);
   }
 
   public showdown(gameState: IGameState): void {}
